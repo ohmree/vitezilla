c50f2ae45f23b4bd9ecd589add9506dfd2616472
ohmree <13455401+ohmree@users.noreply.github.com>
2023-01-04 12:24:30 +0200

    Fix vuepress base url

--- apps/docs/.vuepress/config.ts
+++ apps/docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from './theme';
 import { handleRootAlias } from './utils';
 
 export default defineUserConfig({
-  base: '/',
+  base: '/vitezilla/',
   locales: {
     '/': {
       lang: 'en-US',
